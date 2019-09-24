03721b08315a24f125f3b14a711fbc44407a35db
Ilya Petrov <dnamix1@gmail.com>
2019-09-24 18:39:06 +0300

    homepage broken into smaller reusable react-components, styling menu-items have been finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component.jsx';
+import Homepage from './pages/homepage/homepage.component.jsx';
 
 function App() {
   return (
