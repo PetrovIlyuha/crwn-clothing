b1ac3f5fc08ec1e9d68db62e05c374d5e0b2857c
Ilya Petrov <dnamix1@gmail.com>
2019-09-25 17:28:38 +0300

    custom-button component imbedded

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Homepage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SingInAndSignUpPage from './pages/sing-in-and-sign-up/sing-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SingInAndSignUpPage}/>
       </Switch>
     </div>
   );
---
+++ src/pages/sing-in-and-sign-up/sing-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sing-in-and-sign-up.styles.scss';
+
+const SingInAndSignUpPage = () => (
+  <div className="sing-in-and-sign-up">
+    <SignIn/>
+  </div>
+)
+
+export default SingInAndSignUpPage;
