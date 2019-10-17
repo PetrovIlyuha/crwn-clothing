e14b66abb570df23cb9dc7ccf6ee4d908e8a0f66
Ilya Petrov <dnamix1@gmail.com>
2019-10-17 16:51:52 +0300

    added media-queries to make website more mobile-friendly

--- client/src/components/collections-overview/collections-overview.styles.jsx
+++ client/src/components/collections-overview/collections-overview.styles.jsx
@@ -3,4 +3,11 @@ import styled from 'styled-components';
 export const CollectionsOverviewContainer = styled.div`
   display: flex;
   flex-direction: column;
+   
+  @media screen and (max-width: 800px) {
+    align-items: center;
+    display: grid; 
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 10px;
+  }    
 `;
--- client/src/pages/sing-in-and-sign-up/sing-in-and-sign-up.styles.jsx
+++ client/src/pages/sing-in-and-sign-up/sing-in-and-sign-up.styles.jsx
@@ -5,4 +5,9 @@ export const SignInAndSignUpContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 30px auto;
+
+  @media screen and (max-width: 800px) {
+    display: grid;
+    grid-template-rows: 1fr 1fr; 
+  }
 `;
